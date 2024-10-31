b30654b8273d3f1a0b22d9ecf344e9fa32f41464
Lautaro Camejo <lautarocamejo@gmail.com>
2024-10-31 05:08:00 -0300

    upd general DetalleVenta core function

--- DetalleVenta.cpp
+++ DetalleVenta.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 #include "DetalleVenta.h"
+#include "ArchivoProductos.h"
 DetalleVenta::DetalleVenta()
 {
     productoID = 0;
@@ -19,18 +20,53 @@ DetalleVenta::DetalleVenta(int _productoID, int _cantidadProducto)
     void DetalleVenta :: setCantidadProducto(int _cantidadProducto){cantidadProducto = _cantidadProducto;}
 
     void DetalleVenta :: cargarDetalleDeVenta(){
-        int input;
-        cout<< "Ingrese el ID del producto: " << endl;
-        cin>> input;
-        productoID = input;
-        cout<< "Ingrese la cantidad comprada del producto: " << endl;
-        cin>> input;
-        cantidadProducto = input;
+        ArchivoProductos Productos("ArchivoProductos.dat");
+        Producto registro;
+        int posReg, input;
 
-    }
+
+
+        bool opcionValida = false;
+        while(!opcionValida){
+            for (int i = 0 ; i < Productos.CantidadRegistros();i++ ){
+            registro = Productos.Leer(i);
+             cout<< "i";
+             registro.mostrarProducto();
+             cout<< endl;
+            }
+        cout<< "Ingrese la opcion deseada: ";
+        cin >> posReg;
+            if(posReg > -1 && posReg < Productos.CantidadRegistros()){
+                opcionValida = true;
+                productoID = Productos.Leer(posReg).getProductoID();
+            }else{
+                cout << "Opcion invalida, vuelva a intentarlo"<<endl;
+            }
+        system("pause");
+        system("cls");
+        }
+
+        opcionValida = false;
+        while(!opcionValida){
+            cout<< "Stock Disponible: " << Productos.Leer(posReg).getStock()<<endl<<endl;
+            cout<< "Ingrese la cantidad comprada del producto: " << endl;
+            cin>> input;
+        if(input >0  && input <= Productos.Leer(posReg).getStock()){
+            opcionValida = true;
+            cantidadProducto = input;
+            Productos.Leer(posReg).setStock( 0);
+        }else{
+            cout << "Opcion invalida! Vuelva a intentarlo"<<endl;
+            system("pause");
+            system("cls");
+
+
+        }
+
+
+
+    }}
     void DetalleVenta :: mostrarDetalleDeVenta(){
-    cout<<"-----------------------------------"<<endl;
     cout<< "ID DEL PRODUCTO: " << getProductoID()<<endl;
     cout<< "CANTIDAD DEL PRODUCTO: " << getCantidadProducto()<<endl;
-    cout<<"-----------------------------------"<<endl;
     }
