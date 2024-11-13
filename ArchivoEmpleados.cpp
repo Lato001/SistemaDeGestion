56de63142372531bf698fcfbb86fe262afb96f8d
Benja Romero <benjaromerolima@gmail.com>
2024-11-13 08:44:06 -0300

    upd fix

--- ArchivoEmpleados.cpp
+++ ArchivoEmpleados.cpp
@@ -762,7 +762,7 @@ void ArchivoEmpleados::exportarEmpleadosACSV(string nombreArchivoCSV)
         cout << empleado.getSueldo()<< endl;
         cout << endl;
         Menu::setColor(7);
-        cout << "-----------------------------------------------------" << endl; producto
+        cout << "-----------------------------------------------------" << endl;
         cout << endl;
         cont++;
     }
